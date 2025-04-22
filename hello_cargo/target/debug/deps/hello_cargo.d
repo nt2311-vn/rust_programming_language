0a1663bc3e35ac766899f0b1acac9ccfbd91d615
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-22 11:45:22 +0700

    init cargo project build and run with cargo package manager

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+D:\Work\Project\Git\Rust\rust_programming_language\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+D:\Work\Project\Git\Rust\rust_programming_language\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\Work\Project\Git\Rust\rust_programming_language\hello_cargo\target\debug\hello_cargo.exe: D:\Work\Project\Git\Rust\rust_programming_language\hello_cargo\src\main.rs
