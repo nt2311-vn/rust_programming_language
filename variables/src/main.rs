17cf74a34bdd6017fe7b1c29d58c7ef134469c2d
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-22 21:51:32 +0700

    explore rust data types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let mut x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one = tup.2;
+
+    println!("Each value {five_hundred} {six_point_four} {one}")
 }
