4243b80bbe01b42e41526f2e1e425395561918c8
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-22 16:15:39 +0700

    break if user input correct secret number, remove the print secret number for making the game more fun

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,7 +6,6 @@ fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
     loop {
         println!("Please input your guess.");
 
@@ -23,7 +22,10 @@ fn main() {
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
