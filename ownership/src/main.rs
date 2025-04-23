4dc0dc4018ab59c9797baa4dfb242cb74af0773c
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-23 21:57:49 +0700

    a complex function call to demonstrate rust ownership move and free memory that out of scope

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,17 @@
 fn main() {
-    let s1 = String::from("hello");
-    let _s2 = s1.clone();
-    println!("{s1}")
+    let s1 = gives_ownership();
+
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
+
+    println!("s1 {s1} s3 {s3}");
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }
