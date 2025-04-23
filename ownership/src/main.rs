b3d41fc90f3d6049ac8b7d0862660644d758cb64
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-23 22:33:11 +0700

    return multiple value using tuple

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,17 +1,11 @@
 fn main() {
-    let s1 = gives_ownership();
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
 
-    let s2 = String::from("hello");
-    let s3 = takes_and_gives_back(s2);
-
-    println!("s1 {s1} s3 {s3}");
+    println!("The length of '{s2}' is {len}")
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("yours");
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
