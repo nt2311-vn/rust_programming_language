f012605a051641b181d6c93bace697568327313d
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-23 21:40:14 +0700

    rust never auto create a shallow copy of data, we must explicit use clone to do it. Generally rust will invalidate data when move ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut s = String::from("hello");
-    s.push_str(", world");
-    println!("{s}");
+    let s1 = String::from("hello");
+    let _s2 = s1.clone();
+    println!("{s1}")
 }
