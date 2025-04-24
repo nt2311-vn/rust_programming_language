4b3930f9ba6a449c504a319921cc8074ad8e6d96
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-24 15:49:33 +0700

    we can have struct that have no fields inside, this called unit type struct

--- group_data/src/main.rs
+++ group_data/src/main.rs
@@ -1,26 +1,4 @@
+struct AlwaysEqual;
 fn main() {
-    let user1 = build_user(String::from("email@example.com"), String::from("user123"));
-
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
-
-    println!("{0}", user2.username);
-}
-
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
+    let subject = AlwaysEqual;
 }
