5bea52035024306e536cda9e79628fd51a210962
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-24 14:31:34 +0700

    create an instance of a struct by calling it and assign fields, when we want mutate some fields must define the instance as mut and rust do not allow partial mutate some certain fields in the instance

--- group_data/src/main.rs
+++ group_data/src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
