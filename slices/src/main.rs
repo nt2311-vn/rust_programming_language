688fdbd52fba37e3f5cb0ba160b0f422b1c7fa2b
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-24 13:10:25 +0700

    update function first_word to return slices instead of usize

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -5,14 +5,14 @@ fn main() {
     println!("{word}")
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
