d34988f232c9fe760854fd629c3244f8794de8c5
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-24 14:43:16 +0700

    create instance of struct via function (constructor like)

--- group_data/src/main.rs
+++ group_data/src/main.rs
@@ -1,12 +1,7 @@
 fn main() {
-    let mut user1 = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
-    };
+    let user1 = build_user(String::from("email@example.com"), String::from("user123"));
 
-    user1.email = String::from("anotheremail@example.com");
+    println!("{0}", user1.email);
 }
 
 struct User {
@@ -14,4 +9,13 @@ struct User {
     username: String,
     email: String,
     sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
 }
