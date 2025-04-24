df069195871e10018a113407e708f044b4a8e5e4
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-24 09:03:34 +0700

    instead of moving ownership we can call function with reference without moving

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
     let s1 = String::from("hello");
-    let (s2, len) = calculate_length(s1);
 
-    println!("The length of '{s2}' is {len}")
+    let len = calculate_length(&s1);
+    println!("The length of '{s1}' is {len}");
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len();
-    (s, length)
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
