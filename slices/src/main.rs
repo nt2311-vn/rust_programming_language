14ac82daa3fac34a9de6acd4ac7833074e844528
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-24 11:19:26 +0700

    no problem because first_word is value of len integer why s String and not change its onwership during life time

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {}
+fn main() {
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
 
-fn first_word(s: &String) -> usize {}
+    println!("{word}")
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
