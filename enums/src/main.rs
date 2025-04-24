6a4186cdc6938e2da007498dc19128013b10bff4
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-24 22:12:27 +0700

    each variants in enum can have different amount of associated data

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,14 +1,12 @@
 #[derive(Debug)]
-enum IpAddrKind {
-    V4,
-    V6,
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
-fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
 
-    route(four);
-    route(six);
+fn main() {
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loop_back = IpAddr::V6(String::from("::1"));
 }
 
 fn route(ip_kind: IpAddrKind) {
