ba2219b6cec4c7a6ddf1d602d5eeab1669b18563
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-25 10:31:03 +0700

    cannot do the direct mathematic operation on optional  value with scalar value type

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let some_number = Some(5);
-    let some_char = Some('e');
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
 
-    let absent_number: Option<i32> = None;
+    let sum = x + y;
 }
