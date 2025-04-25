4f8460a535d2db57ef2ea05aaac2f2a86e43807f
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-25 19:57:34 +0700

    we can specify public struct and enums but field of public struct does not mean public as well, we must define it  explicicly

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -13,12 +13,6 @@ mod front_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    front_of_house::hosting::add_to_waitlist();
-}
-
 fn deliver_order() {}
 
 mod back_of_house {
@@ -28,4 +22,27 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // This field is private in public struct
+    // meal.seasonal_fruit = String::from("blueberries");
 }
