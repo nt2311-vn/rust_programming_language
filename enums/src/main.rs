9c8eb8b3edfa09feec0b81b45326be1c1483354e
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-25 09:45:36 +0700

    create an enum conmtain multiple type of associated data tuple, struct, String and none associated data at all.

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,14 +1,15 @@
 #[derive(Debug)]
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
+impl Message {
+    fn call(&self) {}
+}
 fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
-    let loop_back = IpAddr::V6(String::from("::1"));
-}
-
-fn route(ip_kind: IpAddrKind) {
-    println!("`{:?}`", ip_kind)
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
