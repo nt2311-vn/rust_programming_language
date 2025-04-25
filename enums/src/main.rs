5eb23f721c23b892b94c05ce4f8a48492732196d
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-25 10:15:52 +0700

    in rust Option is a enum of None or Some(T) as T is generic value. This None or Some is implemented without te prelude Option so that we can call freely

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,15 +1,6 @@
-#[derive(Debug)]
-enum Message {
-    Quit,
-    Move { x: i32, y: i32 },
-    Write(String),
-    ChangeColor(i32, i32, i32),
-}
-
-impl Message {
-    fn call(&self) {}
-}
 fn main() {
-    let m = Message::Write(String::from("hello"));
-    m.call();
+    let some_number = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
 }
