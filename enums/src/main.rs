b2483850302cb630068ecec957ff32cbbf583945
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-25 11:12:20 +0700

    Option enum (Some and None) are also a commoncase where match expression with none and value arm executed use alot

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -7,6 +7,10 @@ enum Coin {
 
 fn main() {
     value_in_certs(Coin::Penny);
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
 
 fn value_in_certs(coin: Coin) -> u8 {
@@ -18,5 +22,12 @@ fn value_in_certs(coin: Coin) -> u8 {
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
