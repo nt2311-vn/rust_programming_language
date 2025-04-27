39e6310aed47f51cc7336f67018825983eff8882
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-27 21:22:34 +0700

    cargo run to print the vector v, want to mutate a vector must init it with mut

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut v: Vec<i32> = Vec::new();
+    let v_infer = vec![1, 2, 3];
+
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+
+    print!("Vector v: '{:?}", v)
 }
