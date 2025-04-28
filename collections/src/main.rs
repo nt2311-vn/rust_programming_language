10479f1b43851681bbefa316ddd57e12c35f0ae8
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-28 15:50:49 +0700

    concatenate using + , the second param will be reference, the first param will move to variable and cannot be used afterward

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -51,4 +51,10 @@ fn main() {
     s.push_str("bar");
 
     println!("String: {s}");
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
+
+    println!("{s3}")
 }
