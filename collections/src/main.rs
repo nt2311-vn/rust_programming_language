119ce45212b4dba35f593f4128daa8b064276d40
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-28 14:26:15 +0700

    we can access value in vector using get method (return Option reference to value) or using index

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,11 +1,22 @@
 fn main() {
     let mut v: Vec<i32> = Vec::new();
-    let v_infer = vec![1, 2, 3];
+    let v_infer = vec![1, 2, 3, 4, 5];
 
     v.push(5);
     v.push(6);
     v.push(7);
     v.push(8);
 
-    print!("Vector v: '{:?}", v)
+    println!("Vector v: '{:?}", v);
+
+    let third: &i32 = &v_infer[2];
+
+    println!("value of third is {}", *third);
+
+    let third_get: Option<&i32> = v_infer.get(2);
+
+    match third_get {
+        Some(third) => println!("Value of third get is {}", *third),
+        None => println!("There is no third element"),
+    }
 }
