d4179f135db270810b45cee1fd33e30b6508784b
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-29 09:49:51 +0700

    when we use map function with param it will move the ownership to hashmap

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -85,4 +85,12 @@ fn main() {
     for (key, value) in &scores {
         println!("{key}: {value}");
     }
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+
+    println!("{field_name} {field_value}");
 }
