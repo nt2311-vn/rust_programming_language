0a919fa33ad1e05443fc98c1ae620424250b1cb7
nt2311-vn <nguyentoan231196@gmail.com>
2025-04-30 21:51:26 +0700

    rewrite the function using the optional value ?

--- propagating_errors/src/main.rs
+++ propagating_errors/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{self, Read},
+};
+
+fn main() {}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
 }
