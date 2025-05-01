fd1e349aad4cb5a75e4cf590f41d653c2e466ec3
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-01 09:54:06 +0700

    write a function largest to find the max number in a list of number

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,13 +1,18 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
+    let max = largest(&number_list);
 
-    let mut largest = &number_list[0];
+    println!("The largest number is {max}");
+}
 
-    for number in &number_list {
-        if number > largest {
-            largest = number;
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
         }
     }
 
-    println!("The largest number is {largest}");
+    largest
 }
