069227f7a98b43cee3381679f0bf9346554140ba
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-02 11:52:55 +0700

    define T as std lib of partialOrd for comparision

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -4,23 +4,11 @@ fn main() {
     println!("The largest number is {max}");
 
     let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {result}");
 }
 
-fn largest(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-fn largest_char(list: &[char]) -> &char {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
