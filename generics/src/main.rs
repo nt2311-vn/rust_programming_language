2fac3d347c9454d7f1d0c0b2cb9bda8cef2ee30a
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-02 13:18:09 +0700

    struct can define its field as generic as well

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,9 @@
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let max = largest(&number_list);
@@ -6,6 +12,12 @@ fn main() {
     let char_list = vec!['y', 'm', 'a', 'q'];
     let result = largest(&char_list);
     println!("The largest char is {result}");
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+
+    println!("Integer: '{:#?}'", integer);
+    println!("Float: '{:#?}'", float);
 }
 
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
