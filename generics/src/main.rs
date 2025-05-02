8b18e5d6ffc90b7dccb8f0c5089c52574f3ae299
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-02 11:48:15 +0700

    write another find the largest char in a vector of char

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,11 +1,26 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let max = largest(&number_list);
-
     println!("The largest number is {max}");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&char_list);
+    println!("The largest char is {result}");
 }
 
 fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
     let mut largest = &list[0];
 
     for item in list {
