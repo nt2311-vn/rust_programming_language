a924a528fdbc324a3c92428d523df5631540c01b
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-03 21:41:40 +0700

    run project aggregator

--- aggregator/src/main.rs
+++ aggregator/src/main.rs
@@ -1,5 +1,12 @@
 use traits::{Summary, Tweet};
 
 fn main() {
-    let tweet = Tweet {};
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize())
 }
