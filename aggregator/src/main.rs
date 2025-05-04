40d2619c5bd7d5b55d9f618dca9e016c3936b20e
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-04 14:28:57 +0700

    write an instance of newsarticle struct and use println to call method summarize

--- aggregator/src/main.rs
+++ aggregator/src/main.rs
@@ -1,4 +1,4 @@
-use traits::{Summary, Tweet};
+use traits::{NewsArticle, Summary, Tweet};
 
 fn main() {
     let tweet = Tweet {
@@ -8,5 +8,16 @@ fn main() {
         retweet: false,
     };
 
-    println!("1 new tweet: {}", tweet.summarize())
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from(
+            "The Pittsburgh Penguins once again are the best hockey team in the NHL",
+        ),
+    };
+
+    println!("New article available {}", article.summarize());
 }
