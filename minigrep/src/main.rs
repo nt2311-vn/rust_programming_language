61a5138eb077f25d7b0fb365b5c2962fc5f97976
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-06 21:23:59 +0700

    adder rustc info

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -15,28 +15,3 @@ fn main() {
         process::exit(1);
     }
 }
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-    Ok(())
-}
