8e33a6da81cca85f7ca4e081aa73941a810ac422
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-06 14:43:37 +0700

    seperation of concern in binary project create a run function to read the contents in config file path and print to the cmd

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,4 @@
-use std::env;
-use std::fs;
-use std::process;
+use std::{env, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -12,10 +10,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    run(config);
 }
 
 struct Config {
@@ -34,4 +29,11 @@ impl Config {
 
         Ok(Config { query, file_path })
     }
+}
+
+fn run(config: Config) {
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
