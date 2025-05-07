9fbc3b15b2835d2e17270741a01a75745d3aca94
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-07 10:02:43 +0700

    create new test function for case sentive

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -53,4 +53,16 @@ mod tests {
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Pick three.
+            Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
 }
