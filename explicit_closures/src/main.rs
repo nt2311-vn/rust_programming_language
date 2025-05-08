6e75c47fa651a4a600e928f3a1aa2cd3de294e86
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-08 16:29:58 +0700

    create project for explicit closure case and run the project

--- explicit_closures/src/main.rs
+++ explicit_closures/src/main.rs
@@ -1,3 +1,10 @@
+use std::{thread, time::Duration};
 fn main() {
-    println!("Hello, world!");
+    let expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    expensive_closure(5);
 }
