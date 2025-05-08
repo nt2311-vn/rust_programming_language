b22cae76c6210ed31412233cb60858e4be98548d
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-08 21:53:15 +0700

    writting an iterator sum function to test sum method consume iter of vector collection

--- iterator/src/lib.rs
+++ iterator/src/lib.rs
@@ -7,3 +7,12 @@ fn iterator_demonstration() {
     assert_eq!(v1_iter.next(), Some(&2));
     assert_eq!(v1_iter.next(), Some(&3));
 }
+
+#[test]
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.sum();
+    assert_eq!(total, 6);
+}
