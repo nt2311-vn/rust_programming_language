2d0dbb63b7d320f22f0d18a8593e642d09280689
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-08 17:06:49 +0700

    explore the three way of capture environemnts: borrowing immutability, borrowing mutability, take ownership

--- explicit_closures/src/main.rs
+++ explicit_closures/src/main.rs
@@ -7,4 +7,12 @@ fn main() {
     };
 
     expensive_closure(5);
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    let only_borrows = || println!("From closure: {:?}", list);
+    println!("Before calling closure: {:?}", list);
+    only_borrows();
+    println!("After calling closure: {:?}", list);
 }
