ba346c338a467a90a4771fa6382544ec4076bff9
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-27 21:05:15 +0700

    make the compiler run with recursive type when defined in a Box -> the compiler know its has a size of a Box struct

--- smart_pointer/src/main.rs
+++ smart_pointer/src/main.rs
@@ -1,4 +1,13 @@
+#[derive(Debug)]
+enum List {
+    #[allow(dead_code)]
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
 fn main() {
-    let b = Box::new(5);
-    println!("b = {b}")
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("list {:#?}", list)
 }
