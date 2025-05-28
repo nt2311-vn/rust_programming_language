ca1d8d5c2bfafc337e3ba7597183a0888f88e8f7
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-28 20:37:23 +0700

    add c under a scope of executed println after c goes out of scope to see the ref_count

--- reference_counted/src/main.rs
+++ reference_counted/src/main.rs
@@ -16,9 +16,11 @@ fn main() {
     let b = Cons(3, Rc::clone(&a));
     println!("count after creating b = {}", Rc::strong_count(&a));
 
-    let c = Cons(4, Rc::clone(&a));
-    println!("count after creating c = {}", Rc::strong_count(&a));
+    {
+        let _c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
 
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
     println!("b {:#?}", b);
-    println!("c {:#?}", c);
 }
