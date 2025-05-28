02c2e4e316861c32c161edc546a3e64db545aa6e
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-28 16:33:13 +0700

    use strong_count method in Rc to check reference counted when create b and c

--- reference_counted/src/main.rs
+++ reference_counted/src/main.rs
@@ -11,8 +11,13 @@ use std::rc::Rc;
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
 
+    println!("count after creating a  = {}", Rc::strong_count(&a));
+
     let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+
     let c = Cons(4, Rc::clone(&a));
+    println!("count after creating c = {}", Rc::strong_count(&a));
 
     println!("b {:#?}", b);
     println!("c {:#?}", c);
