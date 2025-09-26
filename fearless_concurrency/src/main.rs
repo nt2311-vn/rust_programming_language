34a5b515a51055d5749127790ffaaebbcaaebe5c
nt2311-vn <nguyentoan231196@gmail.com>
2025-09-26 15:41:33 +0700

    ensure both thread are finishing by using handle join

--- fearless_concurrency/src/main.rs
+++ fearless_concurrency/src/main.rs
@@ -1,7 +1,7 @@
 use std::thread;
 use std::time::Duration;
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {i} from the spawn thread!");
             thread::sleep(Duration::from_millis(1));
@@ -12,4 +12,6 @@ fn main() {
         println!("hi number {i} from the main thread!");
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
 }
